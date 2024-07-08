c64bd2578490af95486189bd6ede45c9514b8e93
c-dez <hdezcruzcarlos@gmail.com>
2024-07-08 15:03:20 -0600

    fixed bugs

--- ShootingControlls.gd
+++ ShootingControlls.gd
@@ -2,5 +2,20 @@ extends Node2D
 
 @onready var cross_hair = get_node("CrosshairSprite")
 
-func _process(delta):
+func _ready():
+	cross_hair.visible = false
+
+func _process(_delta):
+	ShootMode()
+
+	pass
+
+func CrosshairPosition():
 	cross_hair.position = get_global_mouse_position()
+	pass
+
+	
+func ShootMode():
+	if Input.is_action_pressed("fire2"):
+		# quiero que aparesca una linea entre el jugador y el crosshair
+		pass
