fcc40fb214d1dd99eae8c221a2ad9df841d7af1d
c-dez <hdezcruzcarlos@gmail.com>
2024-07-10 17:05:58 -0600

    cambie la pocision de origen de draw_line() a bullet_starting.global_position

--- ShootingControlls.gd
+++ ShootingControlls.gd
@@ -2,19 +2,28 @@ extends Node2D
 
 @onready var cross_hair = get_node("CrosshairSprite")
 @onready var player = get_node("../Player")
+
+@onready var bullet_starting = get_node("../Player/BulletArm/BulletStartingPoint")
+
+# parametros drawn_line()
+var color:Color = Color(0,1,0,1)
+var line_width:float = 4.0 
+var anti_alising:bool = false
+
+
 func _ready():
 	pass
-
 func _physics_process(_delta):
 	CrosshairPosition()
 	queue_redraw()
 
 	pass
 
-func CrosshairPosition():
+func CrosshairPosition()->void:
 	cross_hair.position = get_global_mouse_position()
 	pass
 
 	
 func _draw():
-	draw_line(player.position,get_global_mouse_position(), Color(1,0,0,1), -1.0, true)
+	if Input.is_action_pressed("fire2"):
+		draw_line(bullet_starting.get_global_position(), get_global_mouse_position(), color, line_width, anti_alising)
