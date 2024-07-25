2491bafa46ceb9026df5d5b277b02501db9f69ef
c-dez <hdezcruzcarlos@gmail.com>
2024-07-24 18:03:02 -0600

    cambio de color de aim segun si esta o no cargado

--- player/range.gd
+++ player/range.gd
@@ -7,6 +7,7 @@ extends Node2D
 # charge timer
 var charge_time:float = 1
 var charge_time_internal:float = charge_time
+var color := Color(1,1,1,1)
 
 func _ready() -> void:
 	pass
@@ -33,7 +34,7 @@ func draw_aim_line()->void:
 # TODO cuando se esta apuntando reducir velocidad de movimiento de jugador
 
 	if PlayerInputsClass.Mouse_Button_Right_Pressed():
-		draw_dashed_line(position,cross_hair.position,Color(1,1,1,1),5,30, false)
+		draw_dashed_line(position,cross_hair.position,color,5,30, false)
 		
 
 func aim_and_shoot()->void:
@@ -44,7 +45,8 @@ func aim_and_shoot()->void:
 		add_child(a)
 		a.position = global_position
 		
-func charge_countdown(_delta):
+		
+func charge_countdown(_delta)->void:
 # esta funcion se encarga de cargar un spell mas poderoso
 	
 # asigna internal al valor charge timer al presionar aim
@@ -52,14 +54,18 @@ func charge_countdown(_delta):
 		charge_time_internal = charge_time
 # si aim esta presionado y internal es mayor que cero : empieza cuenta regresiva
 	if PlayerInputsClass.Mouse_Button_Right_Pressed() and charge_time_internal > 0:
-		charge_time_internal -=_delta
+		charge_time_internal -= _delta
 # cuando internal es menor que cero boom e internal = 0
 		if charge_time_internal < 0:
+#spell charged
+			color = Color(1,0,0,1)
 			print("boom")
 			charge_time_internal = 0
 # si mientras se mantiene presionado aim y se presiona attack internal se resetea
 	if PlayerInputsClass.Mouse_Button_Right_Pressed() and PlayerInputsClass.Mouse_Button_Left_Just_Pressed():
+#spell discharge
 		charge_time_internal = charge_time
+		color = Color(1,1,1,1)
 	pass	
 
 	 
