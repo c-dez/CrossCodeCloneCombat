4084d33451b20b73837c794e344d6ab685641336
c-dez <hdezcruzcarlos@gmail.com>
2024-07-26 12:21:42 -0600

    cleaned range.gd

--- player/range.gd
+++ player/range.gd
@@ -53,24 +53,24 @@ func aim_and_shoot()->void:
 	
 		
 func charge_countdown(_delta:float)->void:
-# esta funcion se encarga de cargar un spell mas poderoso
-# asigna internal al valor charge timer al presionar aim
+	# esta funcion se encarga de cargar un spell mas poderoso
+	# asigna internal al valor charge timer al presionar aim
 	if Input.is_action_just_pressed(PlayerInputsClass.keys_map.aim):	
 		charge_time_internal = charge_time
-# si aim esta presionado y internal es mayor que cero : empieza cuenta regresiva
+	# si aim esta presionado y internal es mayor que cero : empieza cuenta regresiva
 	if Input.is_action_pressed(PlayerInputsClass.keys_map.aim) and charge_time_internal > 0:
 		charge_time_internal -= _delta
-# cuando internal es menor que cero boom e internal = 0
+		# cuando internal es menor que cero boom e internal = 0
 		if charge_time_internal < 0:
-#spell charged
+			#spell charged
 			color = Color(1,0,0,1)
 			print("boom")
 			# boom code block goes here
 			bullet_damage_mult = mult
 			charge_time_internal = 0
-# si mientras se mantiene presionado aim y se presiona attack, internal se resetea
+	# si mientras se mantiene presionado aim y se presiona attack, internal se resetea
 	if Input.is_action_pressed(PlayerInputsClass.keys_map.aim) and Input.is_action_just_released(PlayerInputsClass.keys_map.attack) or Input.is_action_just_released(PlayerInputsClass.keys_map.aim):
-#spell discharge
+		#spell discharge
 		charge_time_internal = charge_time
 		color = Color(1,1,1,1)
 		bullet_damage_mult = 1
