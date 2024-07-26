a471c98785ef3de8d5d1f1b72ad8adc4a34777fb
c-dez <hdezcruzcarlos@gmail.com>
2024-07-26 12:13:11 -0600

    cambiado la syntaxis de Input

--- player/melee.gd
+++ player/melee.gd
@@ -5,7 +5,7 @@ var damage:int = 30
 # TODO COOLDOWN, ANIMACION
 
 func _physics_process(_delta: float) -> void:
-	if PlayerInputsClass.Mouse_Button_Left_Just_Pressed() and !PlayerInputsClass.Mouse_Button_Right_Pressed():
+	if Input.is_action_just_pressed(PlayerInputsClass.keys_map.attack) and !Input.is_action_just_pressed(PlayerInputsClass.keys_map.aim):		
 		var areas:Array = get_overlapping_bodies()
 		for i:int in areas.size():
 			if "do_damage" in areas[i]:
