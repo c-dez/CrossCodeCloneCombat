9c2389b5d5dfc6e6f033f265384a3f72f6dccb40
c-dez <hdezcruzcarlos@gmail.com>
2024-07-30 21:25:55 -0600

    limpiado

--- bullet_rigid.gd
+++ bullet_rigid.gd
@@ -4,7 +4,7 @@ var speed:int = 30
 var target_pos := Vector2.ZERO
 var timer := Timer.new()
 var angle:float
-var time_out:float = 5
+var time_start:float = 5
 var damage:int = 50
 
 @onready var collision := get_node("Area2D")
@@ -16,26 +16,23 @@ func _ready() -> void:
 	rotation = angle
 	add_child(timer)
 	timer.timeout.connect(_on_timer_timeout)
-	timer.start(time_out)
+	timer.start(time_start)
 	
 	
 func _physics_process(_delta: float) -> void:
-#	ESTO RESUELVE EL PROBLEMA QUE TENIA CON LA VELOCIDAD DE LA BALA SEGUN LA DISTANCIA A LA QUE SE DISPARA
+# ESTO RESUELVE EL PROBLEMA QUE TENIA CON LA VELOCIDAD DE LA BALA SEGUN LA DISTANCIA A LA QUE SE DISPARA
 # con move and collide el vector que le doy de parametro es el angulo(atan2) de la pocicion global de el mouse multiplicado por la velocidad a la que quiero que la bala se mueva
 	move_and_collide(Vector2.from_angle(angle).normalized() * speed)
 	do_damage()
-	pass
 	
 	
 func do_damage()->void:
 	var areas:Array = collision.get_overlapping_bodies()
-# collision 4
+	# collision 4
 	for i:int in areas.size():
-		#if areas[i].has_method("do_damage"):
 		if "do_damage" in areas[i]:
 			areas[i].do_damage(damage)
 			queue_free()
-			pass
 	
 	
 func _on_timer_timeout():
