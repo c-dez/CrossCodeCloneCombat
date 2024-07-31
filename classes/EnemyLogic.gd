da5154417cd83e3da281aeaa384d89702fd41aff
c-dez <hdezcruzcarlos@gmail.com>
2024-07-30 21:09:24 -0600

    este script no es usado por nada

--- classes/EnemyLogic.gd
+++ classes/EnemyLogic.gd
@@ -1,16 +1,16 @@
 class_name EnemyLogic
 extends Node
 
-
-@onready var base_stats = BaseStats.new()
-@onready var damage_numbres = DamageNumbers.new()
-
-var life:int = 100
-
-func _physics_process(_delta):
-	pass
-
-func Damage(damage:int)->void:
-	life -= damage
-	if life <= 0:
-		get_tree().queue_delete(get_parent())
+#NO SE ESTA USANDO ESTE SCRIPT
+#@onready var base_stats = BaseStats.new()
+#@onready var damage_numbres = DamageNumbers.new()
+#
+#var life:int = 100
+#
+#func _physics_process(_delta:float):
+	#pass
+#
+#func Damage(damage:int)->void:
+	#life -= damage
+	#if life <= 0:
+		#get_tree().queue_delete(get_parent())
