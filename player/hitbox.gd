6ec579e3342760a707b1ad55d26e191875a3ef66
c-dez <hdezcruzcarlos@gmail.com>
2024-08-14 00:02:22 -0600

    mejorado deteccion de hitbox en hitbox_behavior()

--- player/hitbox.gd
+++ player/hitbox.gd
@@ -11,22 +11,30 @@ func _ready() -> void:
 	collision.debug_color = hit_box_color_green
 	add_child(inv_frames_timer)
 	inv_frames_timer.timeout.connect(_on_inv_frames_timeout)
-	
+	pass
 	
 	
 func _physics_process(_delta: float) -> void:
+	hitbox_behhavior()
+	pass
+	
+func hitbox_behhavior()->void:
 	if monitoring == true:
 		if has_overlapping_areas():
-			var areas := get_overlapping_areas()
+			var areas:Array = get_overlapping_areas()
+			var filter_area:Array = []
 			for i in areas.size():
 				if "melee_damage" in areas[i]:
-					areas[i].melee_damage()
+					filter_area.append(areas[i])
 					monitoring = false
-					inv_frames_timer.start(inv_frames_cd)
-	
-	pass
-
-func _on_inv_frames_timeout():
+					visible = false
+			if "melee_damage" in filter_area[0]:
+				filter_area[0].melee_damage()
+				inv_frames_timer.start(inv_frames_cd)
+				
+				
+func _on_inv_frames_timeout()->void:
 	monitoring =true
+	visible = true
 	pass
 
